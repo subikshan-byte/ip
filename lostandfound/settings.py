57c85cb521f14f4ce3d15d7133b99a241267e7d1
subikshan <k.subikshandevp@gmail.com>
2026-02-21 21:26:10 +0530

    adding static

--- lostandfound/settings.py
+++ lostandfound/settings.py
@@ -150,3 +150,10 @@ JAZZMIN_SETTINGS = {
     "show_sidebar": True,
     "navigation_expanded": True,
 }
+import os
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
